00743624acf329d848269f06ffad747547178872
ezajacthan <ezajacthan@F1092-18-2106>
2019-09-09 15:16:30 -0500

    Classes added and implementation included but not tested yet

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class DinoNuggets
+{
+    private uint numOfNugs = 6;
+    
+    public double Price { get; set; }
+    public uint Calories { get; set; }
+
+    public List<string> Ingredients
+    {
+        get
+        {
+            List<string> ingredients = new List<string>();
+            for(int i = 0; i>numOfNugs; i++)
+            {
+                ingredients.Add("Chicken Nugget");
+            }
+            return ingredients;
+        }
+    }
+
+    public SteakosaurusBurger()
+    {
+        this.Price = 4.25;
+        this.Calories = (59*numOfNugs);
+    }
+
+    public void AddNugget()
+    {
+        this.Price += .25;
+        this.numOfNugs += 1;
+    }
+}
---
+++ Menu/Entrees/PterodactylWings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class PterodacytlWings
+{
+    public double Price { get; set; }
+    public uint Calories { get; set; }
+
+    public List<string> Ingredients
+    {
+        get
+        {
+            List<string> ingredients = new List<string>() { "Chicken" };
+            ingredients.Add("Wing Sauce");
+            return ingredients;
+        }
+    }
+
+    public Brontowurst()
+    {
+        this.Price = 7.21;
+        this.Calories = 318;
+    }
+
+}
